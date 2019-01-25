c19ab734b0fbdfe0a71aeae1f8bf2f0d047fed67
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-25 22:56:18 +0300

    Основная структура сайта - представления контроллера Home

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -10,5 +10,15 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {   
         public IActionResult Index() => View();
+
+        public IActionResult Shop() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Login() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Checkout() => View();
+        public IActionResult Cart() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Blog() => View();
+        public IActionResult NotFound() => View();
     }
 }
