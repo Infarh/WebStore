aa00f6f7d360168a459b572f24be962c310efd1d
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-26 01:43:22 +0300

    View-компоненты для разделов и брендов

---
+++ WebStore/Models/BrandViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entries.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    public class BrandViewModel : INamedEntry, IOrderedEntry
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        /// <summary>Количества товаров бренда</summary>
+        public int ProductsCount { get; set; }
+
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore/Models/SectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entries.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    public class SectionViewModel : INamedEntry, IOrderedEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        /// <summary>Дочерние секции</summary>
+        public List<SectionViewModel> ChildSections { get; set; } = new List<SectionViewModel>();
+
+        /// <summary>Родительская секция</summary>
+        public SectionViewModel ParentSection { get; set; }
+    }
+}
