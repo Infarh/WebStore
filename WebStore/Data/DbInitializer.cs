ae20d4f550eb9173f9ebd75f4f8030964e12bbfb
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-26 04:09:08 +0300

    Реализация сервиса данных на основе SqlServer+EF источника данных

--- WebStore/Data/DbInitializer.cs
+++ WebStore/Data/DbInitializer.cs
@@ -2,11 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL;
+using WebStore.Entities.Entries;
 
 namespace WebStore.Data
 {
     public class DbInitializer
     {
-   
+        public static void Initialize(WebStoreContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if(context.Products.Any()) return;
+
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                foreach (var section in TestData.Sections) context.Sections.Add(section);
+
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Sections] ON");
+                context.SaveChanges();
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Sections] OFF");
+
+                transaction.Commit();
+            }
+
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                foreach (var brand in TestData.Brands) context.Brands.Add(brand);
+
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Brands] ON");
+                context.SaveChanges();
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+
+                transaction.Commit();
+            }
+
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                foreach (var product in TestData.Products) context.Products.Add(product);
+
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Products] ON");
+                context.SaveChanges();
+                context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[Products] OFF");
+
+                transaction.Commit();
+            }
+        }
     }
 }
---
+++ WebStore/Infrastructure/Implementations/SqlProductData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL;
+using WebStore.Entities.Entries;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    public class SqlProductData : IProductData
+    {
+        private readonly WebStoreContext _DataContext;
+
+        public SqlProductData(WebStoreContext DataContext) => _DataContext = DataContext;
+
+        public IEnumerable<Section> GetSections() => _DataContext.Sections.AsEnumerable();
+
+        public IEnumerable<Brand> GetBrands() => _DataContext.Brands.AsEnumerable();
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter)
+        {
+            if (Filter is null || Filter.SectionId is null && Filter.BrandId is null)
+                return _DataContext.Products.AsEnumerable();
+
+            var query = _DataContext.Products.AsQueryable();
+            if (Filter.BrandId != null)
+                query = query.Where(product => product.BrandId == Filter.BrandId);
+            if (Filter.SectionId != null)
+                query = query.Where(product => product.SectionId == Filter.SectionId);
+
+            return query.AsEnumerable();
+        }
+    }
+}
