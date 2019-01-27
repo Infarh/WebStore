24738e34e305162afbb27aa971e77bffdda1f92a
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-27 19:50:23 +0300

    Разметка админки

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
@@ -11,6 +12,32 @@ namespace WebStore.Areas.Admin.Controllers
 
     public class HomeController : Controller
     {
+        private readonly IProductData _ProductData;
+
+        public HomeController(IProductData ProductData) => _ProductData = ProductData;
+
         public IActionResult Index() => View();
+
+        public IActionResult ProductList() => View(_ProductData.GetProducts(null));
+
+        public IActionResult Details(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
