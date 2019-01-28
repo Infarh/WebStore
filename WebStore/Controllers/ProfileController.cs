db0d6c3e7f258b64c6d656c2a5223eecc3421366
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-28 11:10:21 +0300

    Личный кабинет, заказы

---
+++ WebStore/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly IOrderService _OrderService;
+
+        public ProfileController(IOrderService OrderService) => _OrderService = OrderService;
+
+        public IActionResult Index() => View();
+
+        public IActionResult Orders() =>
+            View(_OrderService.GetUserOrders(User.Identity.Name).Select(order => new UserOrderViewModel
+            {
+                Id = order.Id,
+                Name = order.Name,
+                Address = order.Address,
+                Phone = order.Phone,
+                TotalSum = order.Orders.Sum(i => i.Price * i.Quantity)
+            }));
+    }
+}
---
+++ WebStore/Models/UserOrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace WebStore.Models
+{
+    /// <summary>Модель представления заказа пользователя</summary>
+    public class UserOrderViewModel
+    {
+        /// <summary>Идентификатор</summary>
+        public int Id { get; set; }
+
+        /// <summary>Имя пользователя</summary>
+        public string Name { get; set; }
+
+        /// <summary>Телефон</summary>
+        public string Phone { get; set; }
+
+        /// <summary>Адрес</summary>
+        public string Address { get; set; }
+
+        /// <summary>Сумма заказа</summary>
+        public decimal TotalSum { get; set; }
+    }
+}
