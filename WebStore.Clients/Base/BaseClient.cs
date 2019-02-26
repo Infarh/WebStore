7000084b76d50869588f5113a8c9355e9fa64ccd
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-26 13:01:45 +0300

    Доработка базового клиента WebAPI - добавлены синхронные/асинхронные методы Get,Post,Put,Delete (CRUD-интерфейс)

--- WebStore.Clients/Base/BaseClient.cs
+++ WebStore.Clients/Base/BaseClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace WebStore.Clients.Base
@@ -9,7 +10,7 @@ namespace WebStore.Clients.Base
     public abstract class BaseClient
     {
         /// <summary>HTTP-клиент для организации связи</summary>
-        protected HttpClient _Client;
+        protected readonly HttpClient _Client;
 
         /// <summary>Адрес сервиса</summary>
         protected abstract string ServiceAddress { get; set; }
@@ -22,5 +23,29 @@ namespace WebStore.Clients.Base
             _Client.DefaultRequestHeaders.Accept.Clear();
             _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+
+        #region virtual GET-POST-PUT-DELETE/Async
+
+        protected virtual T Get<T>(string uri) where T : new() => GetAsync<T>(uri).Result;
+        protected virtual async Task<T> GetAsync<T>(string uri) where T : new()
+        {
+            var response = await _Client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsAsync<T>();
+            return new T();
+        }
+
+        protected virtual HttpResponseMessage Post<T>(string uri, T item) => PostAsync(uri, item).Result;
+        protected virtual async Task<HttpResponseMessage> PostAsync<T>(string uri, T item) =>
+            (await _Client.PostAsJsonAsync(uri, item)).EnsureSuccessStatusCode();
+
+        protected virtual HttpResponseMessage Put<T>(string uri, T item) => PutAsync(uri, item).Result;
+        protected virtual async Task<HttpResponseMessage> PutAsync<T>(string uri, T item) =>
+            (await _Client.PutAsJsonAsync(uri, item)).EnsureSuccessStatusCode();
+
+        protected virtual HttpResponseMessage Delete(string uri) => DeleteAsync(uri).Result;
+        protected virtual Task<HttpResponseMessage> DeleteAsync(string uri) => _Client.DeleteAsync(uri); 
+
+        #endregion
     }
 }
