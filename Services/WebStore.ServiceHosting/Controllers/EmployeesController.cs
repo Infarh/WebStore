8267d078e164494de7c7d4a52531c0dacdb67cb2
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-26 12:41:42 +0300

    WebAPI - контроллер управления клиентами

--- Services/WebStore.ServiceHosting/Controllers/EmployeesController.cs
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesController.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Entities.ViewModels;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.ServiceHosting.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmployeesController : ControllerBase
+    [ApiController, Route("api/[controller]"), Produces("application/json")]
+    public class EmployeesController : ControllerBase, IEmployeesData
     {
         private readonly IEmployeesData _EmployeesData;
 
         public EmployeesController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet, ActionName("Get")]
+        public IEnumerable<EmployeeView> GetAll() => _EmployeesData.GetAll();
+
+        [HttpGet("{id}"), ActionName("Get")]
+        public EmployeeView GetById(int id) => _EmployeesData.GetById(id);
+
+        [HttpPost, ActionName("Post")]
+        public void AddNew([FromBody] EmployeeView Employee) => _EmployeesData.AddNew(Employee);
+
+        [HttpPut("{id}"), ActionName("Put")]
+        public EmployeeView Update(int id, [FromBody] EmployeeView Employee) => _EmployeesData.Update(id, Employee);
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => _EmployeesData.Delete(id);
+
+        [NonAction]
+        public void SaveChanges() => _EmployeesData.SaveChanges();
     }
 }
