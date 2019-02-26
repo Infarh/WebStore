58edb85719185e825d0b21f13fd52a39b50e8b49
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-26 13:09:40 +0300

    Клиент WebAPI для доступа к данным Сотрудников

---
+++ WebStore.Clients/Services/Employees/EmployeesClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Entities.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Services.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        protected override string ServiceAddress { get; set; }
+
+        public EmployeesClient(IConfiguration configuration) : base(configuration) => ServiceAddress = "api/employees";
+
+        public IEnumerable<EmployeeView> GetAll() => Get<List<EmployeeView>>(ServiceAddress);
+
+        public EmployeeView GetById(int id) => Get<EmployeeView>($"{ServiceAddress}/{id}");
+
+        public EmployeeView Update(int id, EmployeeView Employee) =>
+            Put($"{ServiceAddress}/{id}", Employee)
+                .Content
+                .ReadAsAsync<EmployeeView>()
+                .Result;
+
+        public void AddNew(EmployeeView Employee) => Post(ServiceAddress, Employee);
+
+        public void Delete(int id) => Delete($"{ServiceAddress}/{id}");
+
+        public void SaveChanges() { }
+    }
+}
