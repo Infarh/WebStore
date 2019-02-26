5a53b24fee1fca43217de2ca1cdc16b03348daea
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-26 12:24:22 +0300

    WebAPI  - добавлен контроллер Employees

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+    }
+}
