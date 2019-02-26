04503aa4f5cb24effc9596b1fbd467877bbae460
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-26 13:21:32 +0300

    Классы для передачи данных (Data-Transfer-Objects - DTO)

---
+++ Common/WebStore.Entities/DTO/BrandDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Entities.Entries.Base.Interfaces;
+
+namespace WebStore.Entities.DTO
+{
+    /// <summary>Объект для передачи данных <see cref="Entries.Brand"/> - бренда</summary>
+    public class BrandDTO : INamedEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Common/WebStore.Entities/DTO/ProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using WebStore.Entities.Entries.Base.Interfaces;
+
+namespace WebStore.Entities.DTO
+{
+    /// <summary>Объект для передачи данных <see cref="Entries.Product"/> - товара</summary>
+    public class ProductDTO : INamedEntry, IOrderedEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        /// <summary>Цена</summary>
+        public decimal Price { get; set; }
+
+        /// <summary>Адрес изображения</summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>Бренд</summary>
+        public BrandDTO Brand { get; set; }
+    }
+}
