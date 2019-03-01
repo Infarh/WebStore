6a9cb338aa8c76787a8b9ccc8b1ed3861ccbeca6
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-01 13:44:14 +0300

    Новый клиент RolesClient

---
+++ WebStore.Clients/Services/RolesClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+
+namespace WebStore.Clients.Services
+{
+    public class RolesClient : BaseClient
+    {
+        public RolesClient(IConfiguration configuration) : base(configuration) => ServiceAddress = "api/Roles";
+    }
+}
