221bfd8d205d2455130f0927dea99a9f2b5d13f4
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-15 18:51:30 +0300

    Таг-хелпер для подсвечивания активного элемента

---
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "is-active-route")]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        private Dictionary<string, string> _RouteValues;
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValues
+        {
+            get => _RouteValues ?? (_RouteValues = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase));
+            set => _RouteValues = value;
+        }
+
+        [HtmlAttributeNotBound, ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            if (ShouldBeActive()) MakeActive(output);
+
+            output.Attributes.RemoveAll("is-active_route");
+        }
+
+        private bool ShouldBeActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+            var currrent_controller = route_values["Controller"].ToString();
+            var current_action = route_values["Action"].ToString();
+
+            const StringComparison str_comp = StringComparison.CurrentCultureIgnoreCase;
+            if (Controller?.Equals(currrent_controller, str_comp) == false) return false;
+            if (Action?.Equals(current_action, str_comp) == false) return false;
+
+            //if (!string.IsNullOrWhiteSpace(Controller) && !string.Equals(Controller, currrent_controller, str_comp))
+            //    return false;
+
+            //if(!string.IsNullOrWhiteSpace(Action) && string.Equals(Action, current_action, str_comp))
+            //    return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+
+            return false;
+        }
+
+        private void MakeActive(TagHelperOutput Output)
+        {
+            const string class_attribute_name = "class";
+            const string active_state = "active";
+            var class_attribute = Output.Attributes.FirstOrDefault(a => a.Name == class_attribute_name);
+            if (class_attribute is null)
+            {
+                class_attribute = new TagHelperAttribute(class_attribute_name, active_state);
+                Output.Attributes.Add(class_attribute);
+            }
+            else if (class_attribute.Value?.ToString().Contains("active", StringComparison.Ordinal) != true)
+                Output.Attributes.SetAttribute(
+                    class_attribute_name, 
+                    class_attribute.Value is null 
+                        ? active_state 
+                        : $"{class_attribute.Value} {active_state}" );
+
+        }
+    }
+}
