8835ea01f3e949a758b7952718f3cf0f05e7927c
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-18 21:19:28 +0300

    ViewComponent корзины

--- UI/WebStore/Components/CartViewComponent.cs
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.Components
 {
-    public class CartViewComponent : ViewComponent
-    {
-        public IViewComponentResult Invoke() => View();
-    }
+public class CartViewComponent : ViewComponent
+{
+    private readonly ICartService _CartService;
+
+    public CartViewComponent(ICartService CartService) => _CartService = CartService;
+
+    public IViewComponentResult Invoke() => View(_CartService.TransformCart());
+}
 }
