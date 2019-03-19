22503d2994c1a916572db49f3aa67f66a5dcae88
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-19 18:47:21 +0300

    Ajax-инкремент количества товаров в позиции корзины

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -13,6 +13,7 @@
 	{
 		$("a.CallAddToCart").click(Cart.addToCart);
 		$(".cart_quantity_delete").click(Cart.removeFromCart);
+		$(".cart_quantity_up").click(Cart.incrementItem);
 	},
 	addToCart: function (event)
 	{
@@ -66,5 +67,33 @@
 			{
 				console.log("removeFromCart error");
 			});
+	},
+	incrementItem: function (event)
+	{
+		const button = $(this);
+		const container = button.closest("tr");
+		event.preventDefault();
+		const id = button.data("id");
+		$.get(Cart._properties.addToCartLink + "/" + id)
+			.done(function ()
+			{
+				const value = parseInt($(".cart_quantity_input").val());
+				$(".cart_quantity_input").val(value + 1);
+				Cart.refreshPrice(container);
+				Cart.refreshCartView();
+			})
+			.fail(function ()
+			{
+				console.log("incrementItem error");
+			});
+	},
+	refreshPrice: function (container)
+	{
+		const quantity = parseInt($(".cart_quantity_input", container).val());
+		const price = parseFloat($(".cart_price", container).data("price"));
+		const totalPrice = quantity * price;
+		const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+		$(".cart_total_price", container).data("price", totalPrice);
+		$(".cart_total_price", container).html(value);
 	}
 };
