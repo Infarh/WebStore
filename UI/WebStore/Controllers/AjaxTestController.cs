22d03454db3431e6c836f9d9125ada887d2f7079
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-03-20 17:30:17 +0300

    Пример асинхронного вызова

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        //https://www.learnrazorpages.com/razor-pages/ajax/unobtrusive-ajax
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> GetTestData()
+        {
+            await Task.Delay(3000);
+            return Content(DateTime.Now.ToString("R"));
+        }
+    }
+}
